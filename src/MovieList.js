d3cede0d0bfd36d4d8a8cf5ffec8c6fa5a19cf90
yasasri <yasasri999@gmail.com>
2024-07-05 23:29:16 +0530

    css modules added

--- src/MovieList.js
+++ src/MovieList.js
@@ -7,6 +7,7 @@ class MovieList extends Component {
     this.state = {
       movies: [
         {
+          id: 1,
           title: "Titanic",
           plot: "The adventurous sink of titanic ship",
           poster:
@@ -18,35 +19,38 @@ class MovieList extends Component {
           isIncart: false,
         },
         {
+          id: 2,
           title: "The Avengers",
           plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
           poster:
             "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
           rating: "8.0",
           price: 99,
-          star: 0,
+          stars: 0,
           fav: false,
           isInCart: false,
         },
         {
+          id: 3,
           title: "The Dark Knight",
           plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
           poster:
             "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
           rating: "9.0",
           price: 199,
-          star: 0,
+          stars: 0,
           fav: false,
           isInCart: false,
         },
         {
+          id: 5,
           title: "Iron Man",
           plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
           poster:
             "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
           rating: "7.9",
           price: 139,
-          star: 0,
+          stars: 0,
           fav: false,
           isInCart: false,
         },
@@ -103,7 +107,8 @@ class MovieList extends Component {
         {movies.map((movie, index) => (
           <Moviecard
             movies={movie}
-            key={index}
+            // key={index}
+            key={movie.id}
             onIncStars={this.handleAddStars}
             onDecStars={this.handleDecStars}
             onClickFav={this.handleToggleFav}
