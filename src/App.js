1715492cdfcc1be91a738fcdee310be9e34ed054
yasasri <yasasri999@gmail.com>
2024-07-09 15:52:47 +0530

    Lifting the state to pass the data

--- src/App.js
+++ src/App.js
@@ -1,14 +1,72 @@
+import React from "react";
 import MovieList from "./MovieList";
 import "./index.css";
 import Navbar from "./Navbar";
+import { movies } from "./movieData";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <MovieList />
-    </>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: movies,
+      cartCount: 0,
+    };
+  }
+  handleAddStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+    if (movies[movieId].stars < 5) {
+      movies[movieId].stars += 0.5;
+    }
+    this.setState({
+      movies,
+    });
+  };
+  handleDecStars = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+    if (movies[movieId].stars > 0) {
+      movies[movieId].stars -= 0.5;
+    }
+    this.setState({
+      movies,
+    });
+  };
+  handleToggleFav = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].fav = !movies[movieId].fav;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  handleAddtocart = (movie) => {
+    const { movies } = this.state;
+    const movieId = movies.indexOf(movie);
+
+    movies[movieId].isIncart = !movies[movieId].isIncart;
+
+    this.setState({
+      movies,
+    });
+  };
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <>
+        <Navbar />
+        <MovieList
+          movies={movies}
+          onIncStars={this.handleAddStars}
+          onDecStars={this.handleDecStars}
+          onClickFav={this.handleToggleFav}
+          onClickAddtocart={this.handleAddtocart}
+        />
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/movieData.js
@@ -0,0 +1,50 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Titanic",
+    plot: "The adventurous sink of titanic ship",
+    poster:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNRxxkYHTzu-T5l-jHg9UjEsMBjvZMXkAlXQ&s",
+    price: 199,
+    rating: 8.8,
+    stars: 0,
+    fav: false,
+    isIncart: false,
+  },
+  {
+    id: 2,
+    title: "The Avengers",
+    plot: "Earth's mightiest heroes must come together and learn to fight as a team if they are going to stop the mischievous Loki and his alien army from enslaving humanity.",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BNDYxNjQyMjAtNTdiOS00NGYwLWFmNTAtNThmYjU5ZGI2YTI1XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_SX300.jpg",
+    rating: "8.0",
+    price: 99,
+    stars: 0,
+    fav: false,
+    isIncart: false,
+  },
+  {
+    id: 3,
+    title: "The Dark Knight",
+    plot: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+    rating: "9.0",
+    price: 199,
+    stars: 0,
+    fav: false,
+    isIncart: false,
+  },
+  {
+    id: 5,
+    title: "Iron Man",
+    plot: "After being held captive in an Afghan cave, billionaire engineer Tony Stark creates a unique weaponized suit of armor to fight evil.",
+    poster:
+      "https://m.media-amazon.com/images/M/MV5BMTczNTI2ODUwOF5BMl5BanBnXkFtZTcwMTU0NTIzMw@@._V1_SX300.jpg",
+    rating: "7.9",
+    price: 139,
+    stars: 0,
+    fav: false,
+    isIncart: false,
+  },
+];
